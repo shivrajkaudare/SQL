bed9b87323d812c302dc896553f7ded9a6ec2587
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-10-24 19:56:47 +0530

    Update DataBase-Relationships.sql

--- DataBase-Relationships.sql
+++ DataBase-Relationships.sql
@@ -1,3 +1,9 @@
 // One to one Relationship : 
 - Primary key of one object passed as foreign key to primary key of another table.
  
+// One to Many Relationship :
+- Each Reacord in Table-A can be associated with multiple records in a table-B, But each record in Table-B can be associated with only one record in Table-A.
+
+
+// Many to Many Relationship :
+- Each Record in a Table-B can be associated with multile record in table-B and vice versa.
