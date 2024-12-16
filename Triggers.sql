a21f8b2a706ae47b4c54c1addebdde7d4d3a1379
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-12-16 18:36:39 +0530

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -1,1 +1,10 @@
-
+/* SQL triggers are essential tools in database management systems (DBMS),
+that allow automatic execution of a set of SQL statements when specific database events,
+such as INSERT, UPDATE, or DELETE operations, occur. */
+/* Syntax */
+create trigger [trigger_name] 
+[before | after]  
+{insert | update | delete}  
+on [table_name]  
+[for each row]  
+[trigger_body] 
